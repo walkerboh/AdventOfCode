2de484985c5a4a3387f38748bd5d9b77892044d1
Evan Frisch <walkerboh1213@gmail.com>
2025-12-01 15:27:12 -0500

    2015 Day 01

---
+++ AdventOfCode2015/Day01.cs
@@ -0,0 +1,57 @@
+﻿using AoCHelper;
+
+namespace AdventOfCode2015
+{
+    internal class Day01 : BaseDay
+    {
+        private readonly string Instructions = string.Empty;
+
+        public Day01()
+        {
+            Instructions = File.ReadAllText(InputFilePath);
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            var floor = 0;
+
+            foreach (var sym in Instructions)
+            {
+                if (sym == '(')
+                {
+                    floor++;
+                }
+                else
+                {
+                    floor--;
+                }
+            }
+
+            return ValueTask.FromResult(floor.ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            var floor = 0;
+
+            for (int i = 0; i < Instructions.Length; i++)
+            {
+                if (Instructions[i] == '(')
+                {
+                    floor++;
+                }
+                else
+                {
+                    floor--;
+                }
+
+                if (floor == -1)
+                {
+                    return ValueTask.FromResult((i + 1).ToString());
+                }
+            }
+
+            return ValueTask.FromResult("error");
+        }
+    }
+}
