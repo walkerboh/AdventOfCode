c23411eaafd650eaa51a0fca10d9c571dc41df77
Evan Frisch <walkerboh1213@gmail.com>
2025-12-01 15:54:01 -0500

    2015 Day 03

---
+++ AdventOfCode2015/Day03.cs
@@ -0,0 +1,79 @@
+﻿using AoCHelper;
+using System.IO.IsolatedStorage;
+using System.Runtime.CompilerServices;
+
+namespace AdventOfCode2015
+{
+    internal class Day03 : BaseDay
+    {
+        private readonly string Instructions = string.Empty;
+
+        public Day03()
+        {
+            Instructions = File.ReadAllText(InputFilePath);
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            var coord = (X: 0, Y: 0);
+            var visited = new HashSet<(int, int)> { coord };
+
+            foreach (var move in Instructions)
+            {
+                coord = MoveCoords(coord, move);
+
+                visited.Add(coord);
+            }
+
+            return ValueTask.FromResult(visited.Count.ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            var santa = (X: 0, Y: 0);
+            var robo = (X: 0, Y: 0);
+            var visited = new HashSet<(int, int)> { santa };
+            var isSanta = true;
+
+            foreach (var move in Instructions)
+            {
+                if (isSanta)
+                {
+                    santa = MoveCoords(santa, move);
+                }
+                else
+                {
+                    robo = MoveCoords(robo, move);
+                }
+
+                visited.Add(isSanta ? santa : robo);
+
+                isSanta = !isSanta;
+            }
+
+            return ValueTask.FromResult(visited.Count.ToString());
+        }
+
+        private static (int X, int Y) MoveCoords((int X, int Y) coord, char dir)
+        {
+            if (dir == '>')
+            {
+                coord.X++;
+            }
+            else if (dir == '<')
+            {
+                coord.X--;
+            }
+            else if (dir == '^')
+            {
+                coord.Y++;
+            }
+            else if (dir == 'v')
+            {
+                coord.Y--;
+            }
+
+            return coord;
+        }
+    }
+}
