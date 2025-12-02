96eb4fbbbe2cc7a2c7288dc8233472fe2c9c7607
Evan Frisch <walkerboh1213@gmail.com>
2025-12-02 14:29:40 -0500

    Added helpers

---
+++ AdventOfCodeHelpers/CustomDay.cs
@@ -0,0 +1,10 @@
+﻿using AoCHelper;
+
+namespace AdventOfCodeHelpers
+{
+    public abstract class CustomDay : BaseDay
+    {
+        protected string GetInputString() => File.ReadAllText(InputFilePath);
+        protected IEnumerable<string> GetInputLines() => File.ReadAllLines(InputFilePath);
+    }
+}
