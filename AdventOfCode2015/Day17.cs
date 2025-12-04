8cd6115398666732d1b67d56bede553161c48784
Evan Frisch <walkerboh1213@gmail.com>
2025-12-04 16:15:30 -0500

    2015 Day 17

---
+++ AdventOfCode2015/Day17.cs
@@ -0,0 +1,28 @@
+﻿
+namespace AdventOfCode2015
+{
+    internal class Day17 : CustomDay
+    {
+        private readonly List<int> Containers;
+        private readonly IEnumerable<IEnumerable<int>> Combos;
+        private readonly int Target = 150;
+
+        public Day17()
+        {
+            Containers = [.. GetInputLines().Select(int.Parse)];
+            Combos = Containers.GetCombinations();
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            return ValueTask.FromResult(Combos.Count(c => c.Sum() == Target).ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            var valid = Combos.Where(c => c.Sum() == Target);
+            var minSize = valid.Min(c => c.Count());
+            return ValueTask.FromResult(valid.Count(v => v.Count() == minSize).ToString());
+        }
+    }
+}
--- AdventOfCodeHelpers/EnumerableExtensions.cs
+++ AdventOfCodeHelpers/EnumerableExtensions.cs
@@ -15,5 +15,27 @@
                     (t1, t2) => t1.Concat(new T[] { t2 }));
         }
 
+        // Source - https://stackoverflow.com/a
+        // Posted by ojlovecd
+        // Retrieved 2025-12-04, License - CC BY-SA 3.0
+
+        public static IEnumerable<IEnumerable<T>> GetCombinations<T>(this List<T> list)
+        {
+            double count = Math.Pow(2, list.Count);
+            for (int i = 1; i <= count - 1; i++)
+            {
+                string str = Convert.ToString(i, 2).PadLeft(list.Count, '0');
+                IEnumerable<T> combo = [];
+                for (int j = 0; j < str.Length; j++)
+                {
+                    if (str[j] == '1')
+                    {
+                        combo = combo.Append(list[j]);
+                    }
+                }
+                yield return combo;
+            }
+        }
+
     }
 }
