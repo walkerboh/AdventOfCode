c5a97be42592151f03d68760e4ec83e636e4eb17
Evan Frisch <walkerboh1213@gmail.com>
2025-12-05 11:20:05 -0500

    2015 Day 20

---
+++ AdventOfCode2015/Day20.cs
@@ -0,0 +1,58 @@
+﻿namespace AdventOfCode2015
+{
+    internal class Day20 : CustomDay
+    {
+        private readonly int Target;
+
+        public Day20()
+        {
+            Target = int.Parse(GetInputString());
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            var houses = new int[Target];
+            Array.Fill(houses, 1);
+            for (var elf = 2; elf < houses.Length; elf++)
+            {
+                for (var house = elf - 1; house < houses.Length; house += elf)
+                {
+                    houses[house] += elf * 10;
+                }
+
+                if (houses[elf - 1] >= Target)
+                {
+                    return ValueTask.FromResult(elf.ToString());
+                }
+            }
+
+            return ValueTask.FromResult("error");
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            var houses = new int[Target];
+            Array.Fill(houses, 1);
+            for (var elf = 2; elf < houses.Length; elf++)
+            {
+                var i = 0;
+                for (var house = elf - 1; house < houses.Length; house += elf)
+                {
+                    houses[house] += elf * 11;
+                    i++;
+                    if (i >= 50)
+                    {
+                        break;
+                    }
+                }
+
+                if (houses[elf - 1] >= Target)
+                {
+                    return ValueTask.FromResult(elf.ToString());
+                }
+            }
+
+            return ValueTask.FromResult("error");
+        }
+    }
+}
