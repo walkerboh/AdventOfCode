af34f2e06e2030678729da7a63c90f52aaf615f7
Evan Frisch <walkerboh1213@gmail.com>
2025-12-08 14:49:04 -0500

    2016 Day 06

---
+++ AdventOfCode2016/Day06.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace AdventOfCode2016
+{
+    internal class Day06 : CustomDay
+    {
+        private readonly List<string> Lines;
+
+        public Day06()
+        {
+            Lines = GetInputLines();
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            var sb = new StringBuilder();
+
+            for(var i = 0; i < Lines[0].Length; i++)
+            {
+                sb.Append(Lines.Select(l => l[i]).GroupBy(x => x).OrderByDescending(x => x.Count()).First().Key);
+            }
+
+            return ValueTask.FromResult(sb.ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            var sb = new StringBuilder();
+
+            for (var i = 0; i < Lines[0].Length; i++)
+            {
+                sb.Append(Lines.Select(l => l[i]).GroupBy(x => x).OrderBy(x => x.Count()).First().Key);
+            }
+
+            return ValueTask.FromResult(sb.ToString());
+        }
+    }
+}
